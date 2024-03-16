c7cbd048e94b722bbfa02a77656a0074ecbccd36
GakuruUri <urigakuru@icloud.com>
2024-03-16 12:37:00 +0300

    Day 72

--- alien_invasion.py
+++ alien_invasion.py
@@ -44,21 +44,35 @@ class AlienInvasion:
                 sys.exit()
                 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-            
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
+                self._check_keyup_events(event)
+                
+                
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True           
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True  
+            
+            
+        elif event.key == pygame.K_q:
+            sys.exit()  
+            
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+                
+    
 
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
 
-                    # Move the ship to the right
-                self.ship.rect.x += 1
+            # Move the ship to the right
+            self.ship.rect.x += 1
 
                 
     

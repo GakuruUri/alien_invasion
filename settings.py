b8912ecb3722c9a2a418d654297b2844b3384e59
GakuruUri <urigakuru@icloud.com>
2024-03-25 21:50:32 +0300

    Day 72 Aliens

--- settings.py
+++ settings.py
@@ -12,6 +12,9 @@ class Settings:
         
         # Alien settings
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # Fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
 
         #Ship settings.
